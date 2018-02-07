853b8acf1c5ad1023ecb5325b86578e71c476e3b
Miguel Pinto <pintossauro@gmail.com>
2018-02-07 13:30:12 +0000

    Added solution for 2nd part of day 2

--- src/day2part2/day2part2.kt
+++ src/day2part2/day2part2.kt
@@ -3,10 +3,21 @@ package day2part2
 import java.io.File
 
 fun main(args: Array<String>) {
-    val spreadsheetLines = File("day2input.txt").readLines()
-    
-    spreadsheetLines.forEach {
+    var sum = 0
+    File("day2input.txt").readLines().map {
         val sorted = it.split("\t").map { it.toInt() }.sortedDescending()
-        println(sorted)
+        sorted.forEach {
+            sum += getSumWithDividend(it, sorted)
+        }
     }
+    println(sum)
+}
+
+fun getSumWithDividend(number: Int, otherNumbers: List<Int>) : Int {
+    otherNumbers.forEach{
+        if (it != number && number % it == 0) {
+            return number / it
+        }
+    }
+    return 0
 }
